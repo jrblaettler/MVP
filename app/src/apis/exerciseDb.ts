d41ce2254f6642708cbb55159a6713aba3b8b502
Jacob Blaettler <jrblaettler@gmail.com>
2022-02-03 15:34:31 -0700

    Changes committed:
    	modified:   app/src/apis/exerciseDb.ts

--- app/src/apis/exerciseDb.ts
+++ app/src/apis/exerciseDb.ts
@@ -4,6 +4,6 @@ export const exerciseDb = axios.create({
   baseURL: 'https://exercisedb.p.rapidapi.com/exercises/',
   headers: {
     'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
-    'x-rapidapi-key': 'd885c71da2msh7e92c472fdad971p1ce4cbjsnb4e35bd75a32',
+    'x-rapidapi-key': 'rapidApiKey',
   },
 });
